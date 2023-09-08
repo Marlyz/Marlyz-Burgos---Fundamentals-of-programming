14a8c33ddb9212353f3d7ea91ae0bbcc1036eed8
Marlyz <m.burgos.barrera.11@student.scu.edu.au>
2023-09-08 14:48:20 +1000

    f2 completed

---
+++ Assessment1.MarlyzB/AssignmentCalculations.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class AssignmentCalculations{
+    
+        public static void main(String [] args){  
+          Scanner scanner = new Scanner(System.in); //system.in catch input from user 
+          
+        //F1:
+          System.out.print("Enter assignemt name: ");  
+          String name = scanner.nextLine();  //receive and store             
+                      
+          
+    
+        //F2:
+            ArrayList<Integer> studentMarks = new ArrayList<>();
+            for (int i = 0; i < 30; i++) {
+                while (true){
+                    try{
+                    System.out.print("Enter the Mark for the assignment " + (i + 1) + ": ");
+                    int studentMark = Integer.parseInt(scanner.nextLine());
+                     // check input as a valid number
+                     if (studentMark >= 0.0 && studentMark <= 30.0){
+                        studentMarks.add(studentMark);
+                      break;
+                    } else {            
+                        System.out.print("Error, Write a valid number :");
+
+                    }
+                    }catch (NumberFormatException e){
+                    System.out.println("Error, Write a valid number: ");
+            }
+        }
+        }
+    }
+}
